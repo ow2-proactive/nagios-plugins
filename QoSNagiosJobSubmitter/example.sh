d2524f2e642da2838f87d56e8433e1dafc6b7270
mjost <mauricio.jost@inria.fr>
2011-10-26 10:23:37 +0200

    [code] Adding the examples to the gitrepo. Signed-off-by: mjost <mauricio.jost@inria.fr>

---
+++ QoSNagiosJobSubmitter/example.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo '*************************************'
+echo Example of usage of the Nagios plugin
+echo '*************************************'
+echo Take a look inside this file!
+echo Remember to set up into the script the following:
+echo    1. the JAVA_HOME where java 1.7 is
+echo    2. Where the plugin is, including job to be executed as the test
+echo    3. Job descriptor path, xml
+echo    4. ProActive configuration file
+echo There are no other special parameters needed. Nagios parameters are optional, -c -w -H.
+echo To see what is going on, you can also add the flag --debug
+echo '*************************************'
+echo Probing, default timeout: 60 seconds...
+./check_job_submission -H hostname -c 100 -w 100 --timeout=60 --debug=3
+./check_job_submission --timeout=300 --debug=3 --jobname=test --hostname=node0 --port=8090
---
+++ QoSNagiosPAMR/example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo '*************************************'
+echo Example of usage of the Nagios plugin
+echo '*************************************'
+echo Take a look inside this file!
+echo Remember to set up into the script the following:
+echo There are no other special parameters needed. Nagios parameters are optional, -c -w -H.
+echo To see what is going on, you can also add the flag --debug
+echo '*************************************'
+echo Probing, default timeout: 60 seconds...
+
+./check_pamr_router -H node0 -t 60
