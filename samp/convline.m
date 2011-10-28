4e60a968333a8974ec42028c897041cc62dd62cb
mjost <mauricio.jost@inria.fr>
2011-10-28 18:49:03 +0200

    [code] Matlab code to generate the curves. Signed-off-by: mjost <mauricio.jost@inria.fr>

--- samp/convline.m
+++
@@ -1,7 +0,0 @@
-function output = convline(input)
-
-output = str2double(struct2cell(input));
-
-% SERVICE STATUS: JOBID 8193 OK | time_initialization=0.003s time_connection=3.889s time_cleaning_old_jobs=0.376s time_submission=0.094s time_execution=1.576s time_output_retrieval=0.078s time_job_removal=0.102s time_disconnection=0.019s timeout_threshold=500.000s time_all_warning_threshold=500.000s time_all=5.761s
-
-
--- samples/readfile.m
+++ samples/readfile.m
@@ -9,7 +9,7 @@ while ischar(tline)
     output11 = readline(tline);
     if (length(output11)==0)
     else
-        output = convline(output11);
+        output = output11;
         if (length(array)==0)
             array = output;
         else
@@ -25,12 +25,20 @@ fclose(fid);
 array = array';
 
 array = [array(:,1:8) array(:,11)];
-
+figure;
 plot(array);
-legend('initi','connec','clean','submis','execu','retriev','removal', 'disconn', 'timeall'); grid
+legend('initi','connec','clean','submis','execu','retriev','removal', 'disconn', 'timeall');
+grid;
 axis([1 size(array,1) 0 max(array(:,9))]);
-
 xlabel('Attemp (in time)');
 ylabel('Time [sec]');
+hold on;
 
-
+first = array(:,1); % Any of them will be infinite if something went wrong. We put a red dot there.
+index = 1;
+for ii=first'
+	if ii==inf
+		plot(index,0,'ro');
+	end
+	index = index + 1;	
+end
