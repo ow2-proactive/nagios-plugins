27ea15ebf229033c011a61e7b2976803ee016a94
mjost <mauricio.jost@inria.fr>
2011-11-02 11:37:58 +0100

    [samples] Measurements obtained during the 4-days-long weekend from the Scheduler's behaviour. Signed-off-by: mjost <mauricio.jost@inria.fr>

--- samples/readfile.m
+++ samples/readfile.m
@@ -10,6 +10,7 @@ while ischar(tline)
     if (length(output11)==0)
     else
         output = output11;
+        array;
         if (length(array)==0)
             array = output;
         else
@@ -26,13 +27,25 @@ array = array';
 
 array = [array(:,1:8) array(:,11)];
 figure;
-plot(array);
+
+nsamples = size(array,1);
+xtime = 1:nsamples;
+
+ColorSet = [1 0.02 0.02; 0.02 1 0.02; 0.02 0.02 1; 1 0.5 0.5; 1 0.02 1; 0.02 1 1; 0.5 0.5 0.02; 0.5 0.02 0.5; 0.02 0.5 0.5];
+set(gca, 'ColorOrder', ColorSet);
+
+hold all;
+
+plot(gca, xtime,array);
+
 legend('initi','connec','clean','submis','execu','retriev','removal', 'disconn', 'timeall');
 grid;
-axis([1 size(array,1) 0 max(array(:,9))]);
-xlabel('Attemp (in time)');
+%axis([1 size(array,1) 0 max(array(:,9))]);
+axis([1 size(array,1) 0 150]);
+xlabel('Attemp (in time) [min x 10]');
 ylabel('Time [sec]');
-hold on;
+titless = strrep(file, '_', ' ');
+title(titless);
 
 first = array(:,1); % Any of them will be infinite if something went wrong. We put a red dot there.
 index = 1;
