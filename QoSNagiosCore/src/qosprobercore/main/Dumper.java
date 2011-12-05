cfa2a07ceacef51f0a5a5f70fefd22eaf1f8e846
mjost <mauricio.jost@inria.fr>
2011-12-05 18:07:28 +0100

    [code] Improved the dumping output. Now it will be more readable. Signed-off-by: mjost <mauricio.jost@inria.fr>

--- QoSNagiosCore/src/qosprobercore/main/Dumper.java
+++ QoSNagiosCore/src/qosprobercore/main/Dumper.java
@@ -8,24 +8,33 @@ import java.io.StringWriter;
 import java.util.Date;
 import org.apache.log4j.Logger;
 
+/**
+ * Class that deals with dumping script execution, if the user asked for it. */
 public class Dumper {
 	private static Logger logger = Logger.getLogger(Dumper.class.getName()); // Logger.
-	private String scriptpath;
-	private String source;
+	private String scriptpath;	// Script to be executed in case of dump.
+	private String source;		// Descriptive string telling what specific probe triggered this dump.
 	
+	/**
+	 * Constructor method.
+	 * @param scriptpath script to be executed for the dump.
+	 * @param source descriptive string telling what specific probe triggered this dump. */
 	public Dumper(String scriptpath, String source){
 		this.scriptpath = scriptpath;
 		this.source = source;
 	}
 	
+	/**
+	 * Start the dump script, giving as its arguments a NagiosReturnObject generated string.
+	 * @param obj NagiosReturnObject to use to generate the arguments for the script. */
 	public void dump(NagiosReturnObject obj){
 		String stacktrace = "";
 		if (obj.getException() != null){
 			stacktrace = getStackTraceAsString(obj.getException());
 			logger.info("Exception obtained converted to '" + stacktrace + "'...");
 		}
-    	String[] command = {scriptpath,  "CODE:[" + obj.getErrorCode() + "]",  "SOURCE:[" + source + "] TIME:[" + (new Date()) + "] MESSAGE:[" + filter(obj.getWholeMessage()) + "] STACKTRACE:[" + filter(stacktrace) + "]"};
-//		logger.info("Dumping with command >" + command + "<...");
+    	String command = scriptpath + " SOURCE:[" + source + "]\nCODE:[" + obj.getErrorCode() + "]\nTIME:[" + (new Date()) + "]\nMESSAGE:[" + filter(obj.getWholeMessage()) + "]\nSTACKTRACE:[" + filter(stacktrace) + "]";
+    	command = filter(command);
 		try {
 			Process p = Runtime.getRuntime().exec(command);
 			InputStream istr = p.getInputStream(); BufferedReader br = new BufferedReader( new InputStreamReader(istr)); String str; while ((str = br.readLine()) != null) logger.info("-         " + str);
@@ -35,7 +44,7 @@ public class Dumper {
 	}
 	
 	private String filter(String source){
-		return source.replace("\n", " /n ").replace("(", " ").replace(")", " ").replace("*", " /a ").replace("|", "  _  ");
+		return source.replace("\n", " /n").replace("(", "/lp").replace(")", "/rp").replace("*", "/as").replace("|", "/pipe");
 	}
 	
     private String getStackTraceAsString(Throwable exception) {
