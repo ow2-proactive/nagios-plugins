7bb5c215217fb90ec84f038777d55803b4fc1535
mjost <mauricio.jost@inria.fr>
2011-12-09 13:40:34 +0100

    [code] Added the debugger probe. Signed-off-by: mjost <mauricio.jost@inria.fr>

---
+++ QoSNagiosDebugger/src/qosprober/main/DebugProber.java
@@ -0,0 +1,154 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2011 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
+
+package qosprober.main;
+
+import org.ow2.proactive.resourcemanager.common.RMState;
+
+import qosprobercore.history.HistoryDataManager;
+import qosprobercore.main.Arguments;
+import qosprobercore.main.PANagiosPlugin;
+import qosprobercore.main.NagiosReturnObject;
+import qosprobercore.main.NagiosReturnObjectSummaryMaker;
+import qosprobercore.main.PAEnvironmentInitializer;
+import qosprobercore.main.TimedStatusTracer;
+
+/** 
+ * This is a general Nagios plugin class that performs a test on the RM, by doing:
+ *    -Node obtaining
+ *    -Node retrieval 
+ *  After that, a short summary regarding the result of the test is shown using Nagios format. */
+public class DebugProber extends PANagiosPlugin{
+	
+	/** 
+	 * Constructor of the prober. The map contains all the arguments for the probe to be executed. 
+	 * @param args arguments to create this RMProber. 
+	 * @throws Exception */
+	public DebugProber(Arguments args) throws Exception{
+		super("DEBUG", args);
+		args.addNewOption("u", "user", true);							// User.
+		args.addNewOption("p", "pass", true);							// Pass.
+		args.addNewOption("r", "url-sched", true);						// Url of the Scheduler.
+		args.addNewOption("R", "url-rm", true);							// Url of the RM.
+		
+	}
+	
+	/**
+	 * Initialize the ProActive environment for this probe. */
+	public void initializeProber() throws Exception{
+		super.initializeProber();
+		PAEnvironmentInitializer.initPAConfiguration(
+			getArgs().getStr("paconf"),
+			getArgs().getStr("hostname"),
+			getArgs().getStr("port"));
+	}
+	
+	/** 
+	 * Validate all the arguments given to this probe. 
+	 * @throws IllegalArgumentException in case a non-valid argument is given. */
+	public void validateArguments(Arguments arguments) throws IllegalArgumentException{
+		super.validateArguments(arguments);
+		arguments.checkIsGiven("url-sched");
+		arguments.checkIsGiven("url-rm");
+		arguments.checkIsGiven("user");
+		arguments.checkIsGiven("pass");
+	}
+	
+	/**
+	 * Probe both scheduler and RM to detect buggy situation.
+	 * @throws Exception */	 
+	public NagiosReturnObject probe(TimedStatusTracer tracer) throws Exception{
+		HistoryDataManager <HistoryElement> historyManager; 
+		try{
+			historyManager = new HistoryDataManager<HistoryElement>("data");
+		}catch(Exception e){
+			return new NagiosReturnObject(NagiosReturnObject.RESULT_3_UNKNOWN, "Could not lock history file, other process has it. Overlapping probes...");
+		}
+		
+		HistoryElement history = historyManager.get(new HistoryElement(0));
+
+		logger.info("History element value: " + history.getData());
+		historyManager.set(new HistoryElement(history.getData() + 1));
+		historyManager.release();
+		
+		RMStubProber rmstub = new RMStubProber();						// We get connected to the RM through this stub. 
+		SchedulerStubProber schedstub = new SchedulerStubProber();		// We get connected to the Scheduler through this stub.
+		
+		rmstub.init(										// We get connected to the RM.
+				getArgs().getStr("url-rm"),  getArgs().getStr("user"), 
+				getArgs().getStr("pass"));	
+		
+		schedstub.init(										// We get connected to the Scheduler.
+				getArgs().getStr("url-sched"),  getArgs().getStr("user"), 
+				getArgs().getStr("pass"));	
+		
+		RMState rmstate = rmstub.getRMState();
+		int freenodes = rmstate.getFreeNodesNumber();	
+		int alivenodes = rmstate.getTotalAliveNodesNumber();	
+		int busynodes = alivenodes - freenodes;	
+		int jobsnumber = schedstub.getAllRunningJobsList().size();							// Get the list of jobs running.
+		
+    	rmstub.disconnect();								// Disconnect from the Resource Manager.
+    	schedstub.disconnect();								// Disconnect from the Scheduler.
+    				
+		NagiosReturnObjectSummaryMaker summary = new NagiosReturnObjectSummaryMaker();  
+		
+		summary.addFact("NODE/S ALIVE=" + alivenodes + " FREE=" + freenodes + " JOB/S RUNNING=" + jobsnumber);
+		
+		if (jobsnumber == 0 && busynodes != 0){
+			summary.addNagiosReturnObject(new NagiosReturnObject(NagiosReturnObject.RESULT_2_CRITICAL, "NO JOBS AND " + busynodes + " BUSY NODES"));
+		}
+		
+		if (summary.isAllOkay() == true){
+			summary.addNagiosReturnObject(new NagiosReturnObject(NagiosReturnObject.RESULT_0_OK, "OK"));
+		}
+		
+		return summary.getSummaryOfAll();
+		
+	}
+
+	/**
+	 * Starting point.
+	 * The arguments/parameters are specified in the file /resources/usage.txt
+	 * @return Nagios error code. */
+	public static void main(String[] args) throws Exception{
+        final Arguments options = new Arguments(args);
+		DebugProber prob = new DebugProber(options);		// Create the prober.
+		prob.initializeProber();
+		prob.startProbeAndExit();							// Start the probe.
+	}
+}	
---
+++ QoSNagiosDebugger/src/qosprober/main/SchedulerStubProber.java
@@ -0,0 +1,119 @@
+/*
+ * ################################################################
+ *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2011 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ * ################################################################
+ * $$PROACTIVE_INITIAL_DEV$$
+ */
+
+package qosprober.main;
+
+import javax.security.auth.login.LoginException;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.log4j.Logger;
+import org.objectweb.proactive.ActiveObjectCreationException;
+import org.objectweb.proactive.core.node.NodeException;
+import org.ow2.proactive.scheduler.common.SchedulerConnection;
+import org.ow2.proactive.scheduler.common.SchedulerState;
+import org.ow2.proactive.scheduler.common.exception.NotConnectedException;
+import org.ow2.proactive.scheduler.common.exception.PermissionException;
+import org.ow2.proactive.scheduler.common.exception.SchedulerException;
+import org.ow2.proactive.scheduler.common.exception.UnknownJobException;
+import org.ow2.proactive.scheduler.common.job.*;
+import org.ow2.proactive.scheduler.common.Scheduler;
+import org.ow2.proactive.authentication.crypto.CredData;
+import org.ow2.proactive.scheduler.common.SchedulerAuthenticationInterface;
+import java.io.IOException;
+import java.security.KeyException;
+import java.util.Vector;
+import org.ow2.proactive.authentication.crypto.Credentials;
+
+/** 
+ * Class that connects the test with the real scheduler, works as a stub. 
+ * This is our interface to the remote Scheduler.
+ * This class is specific for JAVAPA protocol. */
+public class SchedulerStubProber {
+	private static Logger logger =
+			Logger.getLogger(SchedulerStubProber.class.getName()); 		// Logger.
+	private Scheduler schedulerStub; 									// Stub to the scheduler.
+	
+	/**
+	 * Constructor method. */
+	public SchedulerStubProber(){}
+	
+	/** 
+	 * Initialize the connection/session with the scheduler.
+	 * @param protocolStr, protocol to be used to get connected to the scheduler. 
+	 * @param url, url of the scheduler. 
+	 * @param user, username to access the scheduler.
+	 * @param pass, password to access the scheduler.
+	 * */
+	public void init(String url, String user, String pass) throws IllegalArgumentException, LoginException, SchedulerException, KeyException, ActiveObjectCreationException, NodeException, HttpException, IOException{
+		logger.info("Joining the scheduler at '" + url + "'...");
+        SchedulerAuthenticationInterface auth = SchedulerConnection.join(url);
+        logger.info("Done.");
+        logger.info("Logging in...");
+        Credentials cred = Credentials.createCredentials(new CredData(user, pass), auth.getPublicKey());
+        schedulerStub = auth.login(cred);
+        logger.info("Done.");
+	}
+	
+	/** 
+	 * Return the status of the job (running, finished, etc.). 
+	 * @param jobId, the ID of the job. 
+	 * @return the status of the job. */
+	public JobStatus getJobStatus(String jobId) throws NotConnectedException, PermissionException, UnknownJobException, HttpException, IOException{
+		return schedulerStub.getJobState(jobId).getStatus();
+	}  
+	
+	/** 
+	 * Disconnect from the Scheduler. */
+	public void disconnect() throws NotConnectedException, PermissionException, HttpException, IOException{		
+		logger.info("Disconnecting...");
+		schedulerStub.disconnect();
+		logger.info("Done.");
+	}
+	
+	/**
+	 * Get a list of all jobs that are in running queue 
+	 * of the scheduler (all jobs, including my jobs). */
+	public Vector<JobState> getAllRunningJobsList() throws NotConnectedException, PermissionException{
+		logger.info("Getting list of jobs...");
+		SchedulerState st = schedulerStub.getState(false); // Get not only my jobs but all of them.
+		Vector<JobState> vector = st.getRunningJobs();
+		for(JobState js: vector){
+			logger.info("\t" + js.getId() + ":" + js.getName() + " tasks_running=" + js.getNumberOfRunningTasks());
+		}
+		logger.info("Done.");
+		return vector;
+	}
+}
