ba4143bad83a7f47936e9a37cbbae4479704d35d
mjost <mauricio.jost@inria.fr>
2012-03-09 15:25:39 +0100

    [docs] Added the Javadoc generated documentation. Signed-off-by: mjost <mauricio.jost@inria.fr>

--- samples/teste.m
+++
@@ -1,8 +0,0 @@
-readfile('test_performance_shainese_polling');
-readfile('test_performance_shainese_events');
-readfile('test_performance_nodem_polling');
-readfile('test_performance_nodem_events');
-
-saveaspdfall();
-
-close all;
